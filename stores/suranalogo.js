9556933a659e8d3a42130986bf7ea0606409864b
vicentematus <vicentematusortiz@gmail.com>
2022-01-02 17:32:41 -0300

    :art: Ahora se puede scrapear de forma dinamica dandole una cantidad n de urls
    Ahora se puede scrapear dinamicamente. Ahora falta conseguir la paginacion de cada categoria y definirlas.

--- stores/suranalogo.js
+++ stores/suranalogo.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-
+const moment = require('moment');
 let store = {
 	name: 'Sur Analogo',
 	url: 'https://www.suranalogo.cl/',
@@ -44,36 +44,120 @@ const suranalogo = new Suranalogo();
 
 console.log(suranalogo.categories());
 
-// async function surAnalogoScrape(url) {
-// 	const browser = await puppeteer.launch({ headless: true });
-// 	const page = await browser.newPage();
-// 	await page.goto(url);
-// 	console.log('sad');
-// 	const rollos = await page.$$('.col-lg-3.col-md-3.col-6');
+/*Funcion que scrapea solamente la primera pagina, los divs de los products y extrae la informacion de esta.*/
+async function surAnalogoScrape(url) {
+	const browser = await puppeteer.launch({ headless: true });
+	const page = await browser.newPage();
+	await page.goto(url);
 
-// 	for (const rollo of rollos) {
-// 		const rolloName = await rollo.$eval('h4', (el) => el.innerText);
-// 		const rolloPrice = await rollo.$eval(
-// 			'.list-price',
-// 			(el) => el.innerText
-// 		);
-// 		const link = await rollo.$eval('a', (el) => el.href);
+	const rollos = await page.$$('.col-lg-3.col-md-3.col-6');
 
-// 		const film = {
-// 			name: rolloName,
-// 			price: rolloPrice,
-// 			link: link,
-// 		};
+	for (const rollo of rollos) {
+		const rolloName = await rollo.$eval('h4', (el) => el.innerText);
+		const rolloPrice = await rollo.$eval(
+			'.list-price',
+			(el) => el.innerText
+		);
+		const link = await rollo.$eval('a', (el) => el.href);
 
-// 		store.films[0].black_white.push(film);
+		const film = {
+			name: rolloName,
+			price: rolloPrice,
+			link: link,
+		};
 
-// 		console.log(rolloName, rolloPrice, link);
-// 	}
+		store.films.push(film);
 
-// 	console.log(store);
-// 	fs.writeFileSync('json/suranalogo.json', JSON.stringify(store));
+		console.log(rolloName, rolloPrice, link);
+	}
 
-// 	await browser.close();
-// }
+	console.log(store);
+	fs.writeFileSync('json/suranalogo.json', JSON.stringify(store));
 
-// surAnalogoScrape('https://suranalogo.cl/pelicula-b/n-35mm');
+	await browser.close();
+}
+
+/*Funcion que scrapea solamente la primera pagina, los divs de los products y extrae la informacion de esta.*/
+
+async function scrapeProduct(url) {
+	const browser = await puppeteer.launch({ headless: true });
+	const page = await browser.newPage();
+	await page.goto(url);
+	console.log('sad');
+
+	const informacion_div = await page.$(
+		'.form-horizontal',
+		(el) => el.innerHTML
+	);
+
+	const brand = await informacion_div.$eval(
+		'.col-12.brand.hidden-sm-down',
+		(el) => el.innerText
+	);
+
+	const name = await informacion_div.$eval('h1', (el) => el.innerText);
+
+	const description = await informacion_div.$eval(
+		'body > div.container.product-page > div > div.col-lg-4.col-md-5.col-12 > form > div > div:nth-child(3) > div.form-group.row.description > div > div',
+		(el) => el.innerText
+	);
+
+	const condition = 'https://schema.org/OfferItemCondition';
+	const price = await informacion_div.$eval(
+		'#product-form-price-2',
+		(el) => el.innerText
+	);
+
+	const time_stamp = moment().format();
+
+	const currency = 'CLP';
+	const SKU = null;
+	let stock;
+	try {
+		stock = await informacion_div.$eval(
+			'.product-form-stock',
+			(el) => el.innerText || null
+		);
+	} catch (error) {
+		stock = 0;
+	}
+
+	const seller = 'Suranalogo';
+
+	console.log(brand, name);
+	console.log(description);
+	console.log('Precio', price);
+	console.log('time_stamp', time_stamp);
+	console.log('Stock:', stock);
+	console.log('Condition', condition);
+
+	const product = {
+		name: name,
+		brand: brand,
+		description: description,
+		price: price,
+		currency: currency,
+		SKU: SKU,
+		stock: stock,
+		time_stamp: time_stamp,
+		condition: condition,
+		seller: seller,
+	};
+
+	store.films.push(product);
+
+	await browser.close();
+	fs.writeFileSync('json/suranalogo.json', JSON.stringify(store.films));
+}
+
+//surAnalogoScrape('https://suranalogo.cl/pelicula-b/n-35mm');
+
+const urls = [
+	'https://suranalogo.cl/rollo-ilford-fp4-125-35mm-36-exp-blanco-y-negro',
+	'https://suranalogo.cl/promo-3-cu-rollos-carga-pelicula-blanco-y-negro-35mm-kentmere-100-24-exp',
+];
+
+for (let link of urls) {
+	scrapeProduct(link);
+}
+console.log(store.films);
